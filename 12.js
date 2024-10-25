194372930e3d09fed6c9169b3da07baa4d02fde4
Huỳnh Thái Sang <huynhthaisang2011@gmail.com>
2024-10-26 00:54:45 +0700

    update

--- 12.js
+++ 12.js
@@ -1,9 +1,30 @@
 //12. Lớp Product với chiết khấu:
 // • Tạo lớp Product với phương thức getDiscountedPrice() tính giá sau khi áp dụng chiết khấu.
+// tạo 1 mảng rỗng
+//
 class Product {
-  constructor(price, discount) {
-    this.price = price;
-    this.discount = discount;
+  constructor() {
+    this.trans = [];
   }
-  getDiscountedPrice() {}
+  //   constructor(price, discount) {
+  //     this.price = price;
+  //     this.discount = discount;
+  //   }
+  //   getDiscountedPrice() {
+
+  //   }
+  money(id, name) {
+    const a = {
+      id: id,
+      name: name,
+    };
+    this.trans.push(a);
+    console.log(this.trans);
+
+    return this.trans;
+  }
 }
+const use = new Product();
+use.money(1, "bbbbbbb");
+console.log(use.money(1, "sang"));
+console.log(use.money(2, "Longkute"));
